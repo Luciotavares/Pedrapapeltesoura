63069e70b806bbf39689306bbfccf7c0caa32af7
Lucio Gularte <114614664+Luciotavares@users.noreply.github.com>
2022-10-05 23:27:24 +0000

    add lagarto e spock.

--- main.c
+++ main.c
@@ -5,34 +5,37 @@ int main(void) {
   int j1,c1,op;
    do{
          do{
-        printf("Escolha 1.pedra 2.papel 3.tesoura :\n");
+        printf("Escolha 1.pedra 2.papel 3.tesoura 4.Lagarto 5.Spock  :\n");
         scanf("%d",&j1);
-         if(j1<1 || j1>3){
+         if(j1<1 || j1>5){
             printf("opçao errada digite novamente. \n");
           }
-         }while(j1<1 || j1>3);
-     srand( time(NULL));
-      c1=rand()%3;
-      if(c1==1)
-        printf("computador escolheu pedra\n");
-      if(c1==2)
-        printf("computador escolheu papel\n");
-      if(c1==3)
-        printf("computador escolheu tesoura\n");
-      if(j1==c1){
-        printf("empate\n");
-        }
-      if((j1==1 && c1==3) || (j1==2 && c1==1) || (j1==3  && c1==2)){
-        printf("voce ganhou\n");
+         }while(j1<1 || j1>5);
+     srand(time(NULL));
+      c1=rand()%4+1;
+        if(c1==1)
+        printf("computador escolheu pedra.\n");
+        if(c1==2)
+        printf("computador escolheu papel.\n");
+        if(c1==3)
+        printf("computador escolheu tesoura.\n");
+        if(c1==4)
+        printf("computador escolheu Lagarto.\n");
+        if(c1==5)
+        printf("computador escolheu Spock.\n");
+      if((j1==1 && c1==3) || (j1==2 && c1==1) || (j1==3  && c1==2)||(j1==1 && c1==4)||(j1==4 && c1==5)||(j1==3 && c1==4)||(j1==5 && c1==3)||(j1==4 && c1==2)||(j1==2 && c1==5)||(j1==5 && c1==1)){
+        printf("voce ganhou.\n");
       }
-       else{
-        printf("voce perdeu\n");   
+      else if(j1==c1){
+        printf("voce empatou.\n");   
         }
+      else
+        printf("voce perdeu.\n");
         do{
             printf("deseja jogar novamente 1.Sim 2.Não:\n");
             scanf("%d",&op);
           if (op < 1 || op > 2) {
-      printf("voce digitou uma opçao errada \n");
+      printf("voce digitou uma opçao errada. \n");
           }
           }while(op!=1 && op!=2);
     }while(op!=2);
